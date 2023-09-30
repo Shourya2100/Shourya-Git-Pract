e33981a22596569bb5a5e4cec1f53fab9807b18b
Shourya2100 <66374881+Shourya2100@users.noreply.github.com>
2023-09-30 10:14:57 +0100

    modified extracurricular_activities.ts

--- extracurricular_activities.ts
+++ extracurricular_activities.ts
@@ -2,3 +2,7 @@ console.log("I like to play paino.");
 
 console.log("I enjoy taking part in debates");
 
+
+console.log("I want to become school Head Boy");
+
+let activities_taken_part_in : string[] = ['GK Quiz team', 'Debate team', 'Cricket Team','playing Paino','good academics'];
