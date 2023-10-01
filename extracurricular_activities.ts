382558f0810036019335a11ff3f96510ac53cfb1
Shourya2100 <66374881+Shourya2100@users.noreply.github.com>
2023-10-01 23:11:32 +0100

    modified the the file with a message

--- extracurricular_activities.ts
+++ extracurricular_activities.ts
@@ -3,3 +3,5 @@ console.log("I like to play paino.");
 console.log("I enjoy taking part in debates");
 
 console.log("I like to play cricket ");
+
+console.log("Checking fo merging this branch with the master");
